e8641bdddffbee4f1757fb3c6a4b95cbde4e0f94
Bruno <brunovn.eneas@gmail.com>
2024-08-14 23:01:42 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
-import styles from "../style/page.module.css"
-import Imagem from 'next/image';
-import lineguns from '../../public/lineguns.jpg'
+import styles from "../style/page.module.css";
+import Image from 'next/image';
+import lineguns from '../../public/lineguns.jpg';
+import info from '../../public/info.png';
+import upload from '../../public/upload.png';
 
 export default function Home() {
   return (
@@ -11,26 +12,34 @@ export default function Home() {
           <span>LogoValorant</span>
           <span>Comunidade</span>
           <span>Artistas</span>
-          <span className={styles.current}>Envie seu Modelo de Arma</span>
+          <span className={styles.current}>Envie sua Ideia para Nós</span>
         </div>
-        <h1 className={styles.title}>ARMAS</h1>
+        <h1 className={styles.title}>ENVIE SEU DESIGN</h1>
         <div className={styles.container}>
           <div className={styles.leftside}>
             <div className={styles.lcard}>
-                <Image className={styles.main_pincture} src={lineguns} alt="Linha de armas"/>
-                <div className={styles.subtitle}>
-                  <p className={styles.ps}>Linha Skins Primordium</p>
-                  <p className={styles.ps}>Types// Arsenal Completo</p>
-                  <p className={styles.ps}>Esta linda coleção trás um ar mais 'infernal' que pune seus inimigos de forma majestosa</p>
-                </div>
+              <Image className={styles.main_pincture} src={lineguns} alt="Linha de armas" />
+              <div className={styles.subtitle}>
+                <p className={styles.ps}>Linha Skins Primordium</p>
+                <p className={styles.ps}>Types// Arsenal Completo</p>
+                <p className={styles.ps}>Esta linda coleção trás um ar mais 'infernal' que pune seus inimigos de forma majestosa</p>
+              </div>
             </div>
             <button className={styles.sendcard}>ENVIAR</button>
           </div>
           <div className={styles.rcards}>
             <div className={styles.rightside}>
               <div className={styles.line_guns}>
-                <span className={styles.text}>Pesquise pelo modelo da Arma</span>
-                <input className={styles.custom_input} type="search" placeholder="Digite o modelo da arma" />
+                <span className={styles.text}>Pesquise pelo modelo</span>
+                <div className={styles.tooltip}>
+                  <button className={styles.tp}>?</button>
+                  <span className={styles.tooltiptext}>Tooltip text</span>
+                </div>
+                <input
+                  className={styles.custom_input}
+                  type="search"
+                  placeholder="Digite o tipo do modelo"
+                />
               </div>
               <div className={styles.upload}>
                 <form className={styles.form}>
@@ -56,15 +65,15 @@ export default function Home() {
                 <form className={styles.line_guns}>
                   <label className={styles.custom_text_input}>
                     <span className={styles.custom_text}>Titulo:</span>
-                    <input className={styles.custom_inp}/>
+                    <input className={styles.custom_inp} />
                   </label>
                   <label className={styles.custom_text_input}>
                     <span className={styles.custom_text}>Autor:</span>
-                    <input className={styles.custom_inp}/>
+                    <input className={styles.custom_inp} />
                   </label>
                   <label className={styles.custom_text_input}>
                     <span className={styles.custom_text}>Atributos:</span>
-                    <input className={styles.custom_inp}/>
+                    <input className={styles.custom_inp} />
                     <div className={styles.chipsContainer}>
                       <div className={styles.chip}>Exemplo 1</div>
                       <div className={styles.chip}>Exemplo 2</div>
